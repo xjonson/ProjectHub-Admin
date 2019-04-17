61e3c231cc3b5664a38029c9f3eb70c71be24078
jonsonxu <910829960@qq.com>
2019-04-17 11:20:04 +0800

    skill

--- src/app/components/skill/skill.component.ts
+++ src/app/components/skill/skill.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SkillService } from 'src/app/service/skill.service';
+import { ResTpl } from 'src/app/models/ResTpl';
+import { NzMessageService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-skill',
@@ -6,10 +9,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skill.component.scss']
 })
 export class SkillComponent implements OnInit {
+  skillList = []
+  skillName: string
 
-  constructor() { }
+
+  constructor(
+    private skillSrv: SkillService,
+    private nzMessage: NzMessageService,
+  ) { }
 
   ngOnInit() {
+    this.handleGetSkills()
   }
 
+  // 获取全部skill
+  handleGetSkills() {
+    this.skillSrv.getSkills().subscribe(
+      (resTpl: ResTpl) => {
+        this.skillList = resTpl.data
+      }
+    )
+  }
+
+  // 添加skill
+  addSkill() {
+    if (!this.skillName) return
+    const newSkill = {
+      id: +this.skillList[this.skillList.length - 1].id + 1,
+      name: this.skillName
+    }
+    this.skillSrv.addSkill(newSkill).subscribe(
+      (resTpl: ResTpl) => {
+        console.log('resTpl: ', resTpl);
+        if (resTpl.code === 0) {
+          this.skillName = ''
+          this.nzMessage.create('info', `添加成功`);
+          this.handleGetSkills()
+        }
+      }
+    )
+  }
+
+  // 删除skill
+  delSkill(skill) {
+    console.log('skill: ', skill);
+    this.skillSrv.delSkill(skill).subscribe(
+      (resTpl: ResTpl) => {
+        if (resTpl.code === 0) {
+          this.nzMessage.create('info', `删除成功`);
+          this.handleGetSkills()
+        }
+      }
+    )
+  }
 }
--- src/app/service/skill.service.ts
+++ src/app/service/skill.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SkillService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getSkills() {
+    return this.http.get('/api/skill')
+  }
+
+  addSkill(data): Observable<any> {
+    return this.http.post('/api/skill', data)
+  }
+
+  delSkill(data): Observable<any> {
+    return this.http.delete('/api/skill/' + data._id)
+  }
 }
